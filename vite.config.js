8c9372db6b94306f026c9426d9251d4bbc582db7
anujdigitalwork-creator <anuj.digitalwork@gmail.com>
2026-04-14 20:46:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip',
+      threshold: 1024,
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      threshold: 1024,
+    }),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          indianData: ['./src/indianData.js'],
+          asianData: ['./src/asianData.js'],
+          koreanData: ['./src/koreanData.js'],
+          vietnameseData: ['./src/vietnameseData.js'],
+          indonesianData: ['./src/indonesianData.js'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,
+    cssMinify: true,
+    sourcemap: false,
+  },
 })
