77dd3aed25aca940a590c963681c6bc4fb9aec22
anujdigitalwork-creator <anuj.digitalwork@gmail.com>
2026-04-16 11:19:59 +0530

    Update build-rss.js

--- scripts/build-rss.js
+++ scripts/build-rss.js
@@ -1,8 +1,50 @@
 import fs from "fs";
-import { generateRss } from "../src/utils/generateRss.js";
 
-const rss = generateRss();
+const allData = [
+  {
+    dish_name: "Vada Pav",
+    cuisine: "maharashtra",
+    category: "appetizers",
+    short_description: "Iconic Mumbai street food",
+    img: "/images/maharashtra/appetizers/vada-pav.jpg"
+  },
+  {
+    dish_name: "Butter Chicken",
+    cuisine: "indian",
+    category: "main-course",
+    short_description: "Creamy tomato chicken curry",
+    img: "/images/india/main-course/butter-chicken.jpg"
+  }
+];
 
-fs.writeFileSync("./public/rss.xml", rss);
+function slugify(text) {
+  return text.toLowerCase().replace(/\s+/g, "-");
+}
+
+function generateRss() {
+  const items = allData.map((dish) => {
+    const slug = slugify(dish.dish_name);
+
+    return `
+<item>
+<title>${dish.dish_name}</title>
+<link>https://fusionchefy.vercel.app/cuisine/${dish.cuisine}/${dish.category}/${slug}</link>
+<description>${dish.short_description}</description>
+<media:content url="https://fusionchefy.vercel.app${dish.img}" />
+</item>`;
+  }).join("");
+
+  return `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+<title>Fusion Chefy Recipes</title>
+<link>https://fusionchefy.vercel.app</link>
+<description>RSS Feed</description>
+${items}
+</channel>
+</rss>`;
+}
+
+fs.writeFileSync("./public/rss.xml", generateRss());
 
 console.log("RSS generated successfully");
