edc13aeac2dd06fe5cb1ed7fb2c096a22cb9c203
anujdigitalwork-creator <anuj.digitalwork@gmail.com>
2026-04-16 11:16:35 +0530

    Update rssOutput.js

--- src/utils/rssOutput.js
+++ src/utils/rssOutput.js
@@ -1,1 +1,51 @@
+import {
+  indianCuisineData,
+  maharashtraCuisineData,
+  punjabCuisineData,
+  chineseCuisineData,
+  japaneseCuisineData,
+  thaiCuisineData,
+  koreanCuisineData,
+  vietnameseCuisineData,
+  indonesianCuisineData
+} from "../data";
 
+const allData = [
+  ...indianCuisineData,
+  ...maharashtraCuisineData,
+  ...punjabCuisineData,
+  ...chineseCuisineData,
+  ...japaneseCuisineData,
+  ...thaiCuisineData,
+  ...koreanCuisineData,
+  ...vietnameseCuisineData,
+  ...indonesianCuisineData,
+];
+
+function slugify(text) {
+  return text.toLowerCase().replace(/\s+/g, "-");
+}
+
+export function buildRssString() {
+  const items = allData.map((dish) => {
+    const slug = slugify(dish.dish_name);
+
+    return `
+<item>
+<title>${dish.dish_name}</title>
+<link>https://fusionchefy.vercel.app/cuisine/${dish.cuisine.toLowerCase()}/${dish.category.toLowerCase()}/${slug}</link>
+<description>${dish.short_description || ""}</description>
+<media:content url="https://fusionchefy.vercel.app${dish.img}" />
+</item>`;
+  }).join("");
+
+  return `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+<title>Fusion Chefy Recipes</title>
+<link>https://fusionchefy.vercel.app</link>
+<description>Auto RSS Feed</description>
+${items}
+</channel>
+</rss>`;
+}
