368c9985ebfb7b6e00a780146c9a4af336a2c3c7
anujdigitalwork-creator <anuj.digitalwork@gmail.com>
2026-04-16 10:52:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,65 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { compression } from 'vite-plugin-compression2'
-import viteImagemin from 'vite-plugin-imagemin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-
-    // Image optimization
-    viteImagemin({
-      mozjpeg: {
-        quality: 75,
-      },
-      pngquant: {
-        quality: [0.65, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 75,
-      },
-      avif: {
-        quality: 60,
-      },
-    }),
-
-    compression({
-      algorithm: 'gzip',
-      threshold: 1024,
-    }),
-
-    compression({
-      algorithm: 'brotliCompress',
-      threshold: 1024,
-    }),
-  ],
-
-  build: {
-    minify: 'terser',
-
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          indianData: ['./src/indianData.js'],
-          asianData: ['./src/asianData.js'],
-          koreanData: ['./src/koreanData.js'],
-          vietnameseData: ['./src/vietnameseData.js'],
-          indonesianData: ['./src/indonesianData.js'],
-        },
-      },
-    },
-
-    chunkSizeWarningLimit: 500,
-    cssMinify: true,
-    sourcemap: false,
-  },
-})
+  plugins: [react()],
+});
