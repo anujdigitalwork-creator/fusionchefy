103840fcb97a9df3711d7d6e3b014b2ed05ffa6d
anujdigitalwork-creator <anuj.digitalwork@gmail.com>
2026-04-16 11:03:47 +0530

    Update build-rss.js

--- scripts/build-rss.js
+++ scripts/build-rss.js
@@ -1,1 +1,8 @@
+import fs from "fs";
+import { generateRss } from "../src/utils/generateRss.js";
 
+const rss = generateRss();
+
+fs.writeFileSync("./public/rss.xml", rss);
+
+console.log("RSS generated successfully");
